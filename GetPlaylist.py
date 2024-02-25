7a90fd1749fee622e279e46117a73098651b4763
Pankaj-Patil22 <pkjpatil1011@gmail.com>
2024-02-25 23:55:27 +0530

    save each song on next line

--- GetPlaylist.py
+++ GetPlaylist.py
@@ -1,5 +1,42 @@
-import requests 
+import requests, re
 
+
+def save_each_song_on_next_line(input_file_name, output_file_name):
+    print("saving each song on different line")
+    # Define the regular expression pattern
+    pattern = r'\{"id":"[a-zA-Z0-9_-]{8}","title":'
+
+    # Read data from the file
+    output = []
+    count = 0
+    with open(input_file_name, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            matches = re.findall(pattern, line)
+            for match in matches:
+                count = count + 1
+                line = line.replace(match, '\n\n' + match)
+            output.append(line)
+
+    # Write the modified data back to the file
+    with open(output_file_name, 'w', encoding='utf-8') as file:
+        file.writelines(output)
+
+    print("Processing complete.")
+    print(count)
+
+
+def remove_newlines(input_file, output_file):
+    try:
+        with open(input_file, 'r', encoding='utf-8') as file_in:
+            content = file_in.read()
+        # Remove newline characters
+        content = content.replace('\n', '')
+        save_file(content, output_file)
+
+        print("Newlines removed and content written to", output_file)
+    except FileNotFoundError:
+        print("File not found.")
 
 # this function will construct the URL needed for the Get request to get all the songs in the playlist for JioSaavn
 def construct_url_jiosaavn():
@@ -9,11 +46,12 @@ def construct_url_jiosaavn():
     pagination_value = 1
     total_songs = input("Enter total number of songs in your playlist\n")
     total_songs_value = int(total_songs) + 100
+    print("Constructing URL")
     return request_template.format(token=token_value, pagination=pagination_value, total_songs=total_songs_value)
 
 def save_file(content, file_name): 
     try:
-        with open(file_name, 'w') as file:
+        with open(file_name, 'w', encoding='utf-8') as file:
             file.write(content)
         print(f"Content saved successfully to {file_name}")
     except Exception as e:
@@ -21,11 +59,10 @@ def save_file(content, file_name):
 
 def main():
     request_url = construct_url_jiosaavn() 
+    print("Request URL is " + request_url)
     response = requests.get(request_url) # get the playlist
-    save_file(response.text, "playlist_json.txt") 
-    
-
-
+    save_file(response.text, "PlaylistJson.txt") 
+    remove_newlines("PlaylistJson.txt", "RemovedNextLines.txt")
 
 if __name__ == "__main__":
     main()
